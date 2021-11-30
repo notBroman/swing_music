b8b626124d8f2a46a70fc32445f69a8c3d5046b9
notBroman <notbroman@gmail.com>
2021-11-30 22:44:17 +0000

    test passed, can now check if on weekend or not

--- roman.berger/src/main/java/CS2020/assignment2/Utils.java
+++ roman.berger/src/main/java/CS2020/assignment2/Utils.java
@@ -2,9 +2,14 @@ package CS2020.assignment2;
 
 import java.util.*;
 
+import java.time.DayOfWeek;
+import java.time.temporal.ChronoField;
+import java.time.LocalDate;
+import java.time.format.*;
+
 public class Utils
 {
-    public static HashMap<Song> returnSongDurationAndTitleformatted(ArrayList<Song> songList)
+    public static HashMap<UUID, String> returnSongDurationAndTitleformatted(ArrayList<Song> songList)
     {
         /*
          *  returns a sorted hashmap with all the songs in the songList
@@ -27,12 +32,36 @@ public class Utils
             // create formatted String for value from
             // songIter.getTitle() and songIter.getDuration()
             // songIter.getDuration()/60 gives minutes, because int/int = int in java
-            String min_sec = "" + songIter.getDuration()/60 + ":" + songIter.getDuration()%60;
-            String fText = "" + SongIter.getTitle() +"(" + min_sec+ ")";
+            String min_sec = "" + songIter.next().getDuration()/60 + ":" + songIter.next().getDuration()%60;
+            String fText = "" + songIter.next().getTitle() +"(" + min_sec+ ")";
             // add entry to HashMap
-            formattedSongs.put(songIter.getSongID, fText);
+            formattedSongs.put(songIter.next().getSongID(), fText);
         }
 
         return formattedSongs;
+    }
+
+    public static boolean checkIfBornOnWeekend(String dob)
+    {
+        /*
+         * a function that determines if a date is on a weekend
+         *
+         * uses the time module of the java SDK
+         */
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        LocalDate birthday = LocalDate.parse(dob, formatter);
+        DayOfWeek day = DayOfWeek.of(birthday.get(ChronoField.DAY_OF_WEEK));
+
+        switch(day)
+        {
+            case SATURDAY:
+                return true;
+
+            case SUNDAY:
+                return true;
+
+            default:
+                return false;
+        }
     }
 }
