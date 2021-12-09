7c985ec2dab8bffea022df270e51dbc21d7690c6
notBroman <notbroman@gmail.com>
2021-12-09 22:37:06 +0000

    more tests for util class

--- roman.berger/src/test/java/CS2020/assignment2/UtilsTest.java
+++ roman.berger/src/test/java/CS2020/assignment2/UtilsTest.java
@@ -18,11 +18,50 @@ public class UtilsTest
     }
 
     @Test
+    public void isWeekend1()
+    {
+        try
+        {
+            assertTrue(Utils.checkIfBornOnWeekend("12 Mar 2022"));
+        }
+        catch(Exception e)
+        {
+            System.out.println(e);
+        }
+    }
+
+    @Test
     public void isWeekday()
     {
         try
         {
             assertFalse(Utils.checkIfBornOnWeekend("2021 Nov 30"));
+        }
+        catch(Exception e)
+        {
+            System.out.println(e);
+        }
+    }
+
+    @Test
+    public void isWeekday1()
+    {
+        try
+        {
+            assertFalse(Utils.checkIfBornOnWeekend("2021-Dec-24"));
+        }
+        catch(Exception e)
+        {
+            System.out.println(e);
+        }
+    }
+
+    @Test
+    public void isWeekday2()
+    {
+        try
+        {
+            assertFalse(Utils.checkIfBornOnWeekend("1 Feb 2021"));
         }
         catch(Exception e)
         {
